fc61e3dde1d93dd978d011c0588ad80bcc81f113
Febry Ardiansyah <febrymuhammad80@gmail.com>
2024-05-05 12:27:24 +0700

    Initial Commit

---
+++ AirBnb-Clone-SwiftUI/AirBnb_Clone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnb_Clone_SwiftUIApp.swift
+//  AirBnb-Clone-SwiftUI
+//
+//  Created by Febry Ardiansyah on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnb_Clone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
